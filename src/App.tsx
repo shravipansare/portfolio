577fec3cef82d420d1e96fd3d2abc22b82b617f5
Shravani <shravanipansare80@gmail.com>
2026-08-04 16:49:37 +0530

    fix: ts import error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useRef, useEffect, KeyboardEvent } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import type { KeyboardEvent } from 'react';
 import './index.css';
 
 interface HistoryItem {
